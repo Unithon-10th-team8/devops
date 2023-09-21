f5ca914273b1aef3a11702813329ee606cf53de0
hyeonwoojo <h_jo@ejn.gg>
2023-09-21 19:58:36 +0900

    feat: modify node pool name

--- terraform/cluster.tf
+++ terraform/cluster.tf
@@ -2,13 +2,14 @@ resource "vultr_kubernetes" "k8" {
   region = "icn"
   label     = "unithon-cluster"
   version = "v1.27.2+1"
+}
 
-  node_pools {
-    node_quantity = 3
-    plan = "vc2-2c-4gb"
-    label = "main-node-pool"
-    auto_scaler = true
-    min_nodes = 3
-    max_nodes = 5
-  }
+resource "vultr_kubernetes_node_pools" "k8_node_pool" {
+  cluster_id = vultr_kubernetes.k8.id
+  node_quantity = 3
+  plan = "vc2-2c-4gb"
+  label = "unithon-main-node"
+  auto_scaler = true
+  min_nodes = 3
+  max_nodes = 5
 }
